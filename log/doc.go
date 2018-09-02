0adc5aa00412c96b0a6d0c76c9cfece2c15659ef
Alan Bunjevac <alan.bunjevac@exobyte.org>
2018-09-02 14:10:28 +0200

    Package docs

---
+++ log/doc.go
@@ -0,0 +1,12 @@
+// Package log provides simple logging framework using informational and debugging messages only. This package provides
+// a rolling logger also.
+//
+// This implementation is inspired by Dave Cheney's post:
+//   https://dave.cheney.net/2015/11/05/lets-talk-about-logging
+//
+// Expected usage is to:
+//  - configure application-wide rolling logger using InstallLogger() method
+//  - log informational and debug messages using "log" package
+//
+// See "debug" package for setting debug mode on or off.
+package log
